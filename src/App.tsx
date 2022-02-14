56c1fa148a745e75fcbb580181943902baf37278
kainy ryu <kainy2109@gmail.com>
2022-02-14 10:04:10 +0900

    Add the projects main specs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from './pages/Main';
 
+// - 지역 리스트는 hard coding
+//   - 지역이름, 좌표값
+// - 페이지 랜딩시 현 시간, 특정 지역 날씨 정보 보여주기
+// - 랜딩페이지 -> 지역 리스트 클릭 했을 때 날씨 정보 갱신
+// - 지역 추가
+//   - 각자의 방식으로 저장
+// - 지역 삭제
+
 function App() {
   return (
     <div className="App">
