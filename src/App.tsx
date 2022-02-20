417c8d0522487edcefcde41f80dab6bc673a3a91
kainy ryu <kainy2109@gmail.com>
2022-02-21 08:43:28 +0900

    first test case succeed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import './App.css';
-import Main from './pages/Main';
+// import Main from './pages/Main';
+import logo from './logo.svg';
 
 // - 지역 리스트는 hard coding
 //   - 지역이름, 좌표값
@@ -14,7 +14,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Main />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
