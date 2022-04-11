f5b5655ff8d01037fcba9bc24f066e2044be918b
kainy ryu <kainy2109@gmail.com>
2022-04-11 09:39:23 +0900

    list onclick button

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,22 +1,29 @@
-import Styled from 'styled-components';
-import { CITIES } from 'data';
+import React, { useEffect } from 'react';
+import { getDataFromLocalStorage, setLocalStorage } from 'helper';
+import { CITY } from 'data';
 
 export default function List() {
+  useEffect(() => {
+    setLocalStorage('locationList', JSON.stringify(CITY));
+  }, []);
+
+  const getItem: any = getDataFromLocalStorage('locationList');
+  const list: string[] = JSON.parse(getItem);
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+  };
+
   return (
-    <StyledList>
-      {CITIES.map(({ location }) => {
-        return <StyledButton name={location}>{location}</StyledButton>;
-      })}
-    </StyledList>
+    <div>
+      <ul>
+        {list?.map((city: any, idx: number) => (
+          <li key={idx}>
+            <button onClick={onClick}>{city}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
-
-const StyledList = Styled.div`
-  display: flex;
-  flex-flow: column;
-  padding-top: 50px;
-`;
-
-const StyledButton = Styled.button`
-  margin: 10px;
-`;
