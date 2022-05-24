6c2754be5466cdc11919fe89ae2a1ffab608f156
Kainy Ryu <kainy2109@gmail.com>
2022-05-24 21:44:05 +0900

    create a helper file in lib folder to manage localstorage

---
+++ src/lib/helper.ts
@@ -0,0 +1,9 @@
+interface LocalStorageProps {
+  readonly key: string;
+  readonly value?: {};
+}
+
+export const getLocalStorage = (key: string) => localStorage.getItem(key);
+
+export const setLocalStorage = ({ key, value }: LocalStorageProps) =>
+  localStorage.setItem(key, JSON.stringify(value));
